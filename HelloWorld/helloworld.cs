24dbf71db91953602390723fa1069acf4cefafc2
Andrew Sears <andrewjsears@gmail.com>
2016-12-24 06:15:30 -0500

    init

---
+++ HelloWorld/helloworld.cs
@@ -0,0 +1,14 @@
+using System;  
+
+class HelloWorld  
+{  
+    static void Main()  
+    {  
+#if DebugConfig  
+        Console.WriteLine("WE ARE IN THE DEBUG CONFIGURATION");  
+#endif  
+
+        Console.WriteLine("Hello, world!");  
+    }  
+}  
+
